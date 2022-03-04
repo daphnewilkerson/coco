0af9f861c233585850ed74eadb498811c3d304e1
savpayne <64240916+savpayne@users.noreply.github.com>
2022-03-03 22:15:06 -0800

    added coco page w little on it

--- coco-react-native/components/Coco.js
+++ coco-react-native/components/Coco.js
@@ -1,12 +1,40 @@
-import React from 'react';
-import { View, Text, StyleSheet} from 'react-native';
+import 'react-native-gesture-handler';
+import * as React from 'react';
+import {
+  useContext
+} from 'react';
+import { Component } from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { useNavigation } from '@react-navigation/native';
+import { StyleSheet, Text, View, Image, Pressable } from 'react-native';
+import { COLORS, MAINFONT, UserContext, dogimages } from '../utils/constants';
 
-export default function Profile() {
+
+export default function Landing() {
+
+  const navigator = useNavigation();
+  const { dogNum } = useContext(UserContext);
+
   return (
     <View style={styles.container}>
-      <Text>
-        CoCo Page!
+      <Text style={styles.title}>
+        CoCo
       </Text>
+      <Pressable style={styles.landingButton} >
+        <Text style={styles.landingButtonText}>Customize CoCo</Text>
+      </Pressable>
+      <Pressable style={styles.landingButton} >
+        <Text style={styles.landingButtonText}>CoCo Bones</Text>
+      </Pressable>
+      <Image
+        style={[styles.dog, {
+          transform: [
+            { rotate: "45deg" }
+          ]
+        }]}
+        source={dogimages[dogNum]}
+      />
     </View>
   )
 }
@@ -14,8 +42,35 @@ export default function Profile() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#f9f9f1ff',
+    backgroundColor: COLORS.background,
+    alignItems: 'center',
+    paddingTop: 100
+  },
+  title: {
+    color: COLORS.darkGreen,
+    fontFamily: MAINFONT,
+    fontSize: 45,
+    marginBottom: 100
+  },
+  landingButton: {
+    backgroundColor: COLORS.lightGreen,
+    width: '70%',
+    height: 80,
     alignItems: 'center',
     justifyContent: 'center',
+    borderRadius: 50,
+    margin: 15,
   },
+  landingButtonText: {
+    color: COLORS.darkGreen,
+    fontFamily: MAINFONT,
+    fontSize: 20,
+  },
+  dog: {
+    height: 250,
+    resizeMode: 'contain',
+    marginTop: 'auto',
+    marginBottom: -3,
+    right: 180,
+  }
 });
--- coco-react-native/components/Profile.js
+++ coco-react-native/components/Profile.js
@@ -1,6 +1,9 @@
 import 'react-native-gesture-handler';
 import * as React from 'react';
-import { Component, useContext } from 'react';
+import {
+  useContext
+} from 'react';
+import { Component } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { useNavigation } from '@react-navigation/native';
